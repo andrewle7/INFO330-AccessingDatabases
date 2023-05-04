48f78147e93c59ccd0ea661c9d553fc8ff0a5071
andrewle7 <ale527@uw.edu>
2023-05-03 22:54:03 -0700

    finished with ec

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,34 +1,84 @@
-import sqlite3  # This is the package for all sqlite3 access in Python
-import sys      # This helps with command-line parameters
+import sqlite3
+import sys
 
-# All the "against" column suffixes:
-types = ["bug","dark","dragon","electric","fairy","fight",
-    "fire","flying","ghost","grass","ground","ice","normal",
-    "poison","psychic","rock","steel","water"]
+connection = sqlite3.connect("/Users/andrewle/Documents/School/INFO330/INFO330-AccessingDatabases/pokemon.sqlite")
+cursor = connection.cursor()
 
-# Take six parameters on the command-line
+types = ["bug", "dark", "dragon", "electric", "fairy", "fight",
+         "fire", "flying", "ghost", "grass", "ground", "ice", "normal",
+         "poison", "psychic", "rock", "steel", "water"]
+
 if len(sys.argv) < 6:
     print("You must give me six Pokemon to analyze!")
     sys.exit()
 
 team = []
+
 for i, arg in enumerate(sys.argv):
     if i == 0:
         continue
+    # checks to see if input is a number or name (EXTRA CREDIT)
+    if arg.isdigit():
+        pokedex_number = int(arg)
+        # query to select the name of the pokemon if user gives an id and saves it
+        cursor.execute("SELECT name FROM pokemon WHERE id=?", (pokedex_number,))
+        pokemon = cursor.fetchone()
+        pokemon_name = pokemon[0]
+    else:
+        pokemon_name = arg
+        # queries pokedex_number if given a name
+        cursor.execute("SELECT id FROM pokemon WHERE name=?", (pokemon_name,))
+        pokemon = cursor.fetchone()
+        pokedex_number = pokemon[0]
 
-    # Analyze the pokemon whose pokedex_number is in "arg"
+    team.append(pokedex_number)
 
-    # You will need to write the SQL, extract the results, and compare
-    # Remember to look at those "against_NNN" column values; greater than 1
-    # means the Pokemon is strong against that type, and less than 1 means
-    # the Pokemon is weak against that type
+    # selects type id from the given pokemon and saves it.
+    cursor.execute("SELECT type_id, which FROM pokemon_type WHERE pokemon_id=?", (pokedex_number,))
+    type_rows = cursor.fetchall()
+    type_ids = [row[0] for row in type_rows] #extracts first element in each row, in this case being type_id
+    type_names = []
+
+    # fetches types according to it's type_id
+    for type_id in type_ids:
+        cursor.execute("SELECT name FROM type WHERE id=?", (type_id,))
+        type_names.append(cursor.fetchone()[0])
+    strong_against = []
+    weak_against = []
+
+    # outer loop iterates through the pair of type_names and type_ids
+    for poke_type, type_id in zip(type_names, type_ids):
+        against_values = []
+
+        # inner loop fetches the against value and appends it to against_values list with the corresponding type_name
+        for against_type in types:
+            column_name = f"against_{against_type}"
+            cursor.execute(f"SELECT {column_name} FROM against WHERE type_source_id1=? OR type_source_id2=?", (type_id, type_id))
+            against_value = cursor.fetchone()[0]
+            against_values.append((against_type, against_value))
+
+        # loop iterates through the against_values list and appends values to weak or strong against based off effectiveness
+        for against_type, against_value in against_values:
+            if against_value < 1:
+                strong_against.append(against_type)
+            elif against_value > 1:
+                weak_against.append(against_type)
+
+    strong_against = list(set(strong_against))
+    weak_against = list(set(weak_against))
+
+    # printing output from readme example
+    print(f"Analyzing {pokedex_number}")
+    print(f"{pokemon_name} ({' '.join(type_names)}) is strong against {strong_against} but weak against {weak_against}")
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
-    teamName = input("Enter the team name: ")
+    team_name = input("Enter the team name: ")
+    connection.commit()
 
-    # Write the pokemon team to the "teams" table
-    print("Saving " + teamName + " ...")
+    print(f"Saving {team_name} ...")
 else:
     print("Bye for now!")
 
+cursor.close()
+connection.close()
